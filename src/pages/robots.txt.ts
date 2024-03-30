61263abcbbdc4758f8c8790b6ad252e912933ac0
liruifengv <liruifeng1024@gmail.com>
2024-03-30 08:56:26 +0800

    fix format

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -7,10 +7,12 @@ Disallow:
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL("sitemap-index.xml", "https://astro-showcase-cn.zeabur.app").href}
+Sitemap: ${
+	new URL("sitemap-index.xml", "https://astro-showcase-cn.zeabur.app").href
+}
 `.trim();
 
 export const GET: APIRoute = () =>
-  new Response(robots, {
-    headers: { "Content-Type": "text/plain" },
-  });
+	new Response(robots, {
+		headers: { "Content-Type": "text/plain" },
+	});
