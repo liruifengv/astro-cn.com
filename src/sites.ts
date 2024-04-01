d8c46b02a81878c57965177eca1e229a4a701c62
XLor <yjl9903@vip.qq.com>
2024-04-01 21:07:44 +0800

    refactor: move out of sites data

---
+++ src/sites.ts
@@ -0,0 +1,142 @@
+export interface Site {
+  title: string;
+
+  href: string;
+
+  thumbnail: string;
+}
+
+export const sites: Site[] = [
+  {
+    title: 'liruifengv.com',
+    href: 'https://liruifengv.com',
+    thumbnail: 'liruifengv.com.png',
+  },
+  {
+    title: 'captainofphb.me',
+    href: 'https://captainofphb.me',
+    thumbnail: 'captainofphb.me.png',
+  },
+  {
+    title: 'Sehnsucht',
+    href: 'https://blog.sehnsucht.top',
+    thumbnail: 'blog.sehnsucht.top.png',
+  },
+  {
+    title: "Kai's blog",
+    href: 'https://kaiyi.cool/',
+    thumbnail: 'kaiyi.cool.png',
+  },
+  {
+    title: "Mikan's Home",
+    href: 'https://blog.bangdream.moe',
+    thumbnail: 'blog.bangdream.moe.png',
+  },
+  { title: 'yuhang.ch', href: 'https://yuhang.ch', thumbnail: 'yuhang.ch.png' },
+  {
+    title: 'blog.pakchoi.opscat.cn',
+    href: 'https://blog.pakchoi.opscat.cn',
+    thumbnail: 'blog.pakchoi.opscat.cn.png',
+  },
+  {
+    title: 'blog.asyncx.top',
+    href: 'https://blog.asyncx.top',
+    thumbnail: 'blog.asyncx.top.png',
+  },
+  {
+    title: '四畳半のへや',
+    href: 'https://blog.moeyua.com',
+    thumbnail: 'blog.moeyua.com.png',
+  },
+  {
+    title: 'Bruce の Site',
+    href: 'https://brucesong.xyz',
+    thumbnail: 'brucesong.xyz.png',
+  },
+  {
+    title: '左子祯',
+    href: 'https://zuozizhen.com',
+    thumbnail: 'zuozizhen.com.png',
+  },
+  {
+    title: 'devlike.top',
+    href: 'https://devlike.top',
+    thumbnail: 'devlike.top.png',
+  },
+  {
+    title: "Licodeao's Blog",
+    href: 'https://licodeao.netlify.app',
+    thumbnail: 'licodeao.netlify.app.png',
+  },
+  {
+    title: 'lolo洛明',
+    href: 'https://blog.luoming.space',
+    thumbnail: 'blog.luoming.space.png',
+  },
+  {
+    title: "Volare's blog",
+    href: 'https://hanzhen.wang',
+    thumbnail: 'hanzhen.wang.png',
+  },
+  {
+    title: '超级小镇 · 刘俊',
+    href: 'https://super.town',
+    thumbnail: 'super.town.png',
+  },
+  { title: '面条实验室', href: 'https://mt.ci', thumbnail: 'mt.ci.png' },
+  {
+    title: 'chi.miantiao.me',
+    href: 'https://chi.miantiao.me',
+    thumbnail: 'chi.miantiao.me.png',
+  },
+  {
+    title: 'huyikai.xyz',
+    href: 'https://huyikai.xyz',
+    thumbnail: 'huyikai.xyz.png',
+  },
+  {
+    title: 'File Renamer Online',
+    href: 'https://renamer.forth.ink',
+    thumbnail: 'renamer.forth.ink.png',
+  },
+  {
+    title: 'ssshooter.com',
+    href: 'https://ssshooter.com',
+    thumbnail: 'ssshooter.com.png',
+  },
+  {
+    title: 'JoeSay',
+    href: 'https://joesay.pages.dev',
+    thumbnail: 'joesay.pages.dev.png',
+  },
+  {
+    title: '小付同学的开发日常',
+    href: 'https://blog.xiaozi.cc',
+    thumbnail: 'blog.xiaozi.cc.png',
+  },
+  {
+    title: 'Anime Garden',
+    href: 'https://garden.onekuma.cn',
+    thumbnail: 'garden.onekuma.cn.png',
+  },
+  {
+    title: '龙场茶室',
+    href: 'https://blog.peterchen97.cn',
+    thumbnail: 'blog.peterchen97.cn.png',
+  },
+  {
+    title: '人生現役',
+    href: 'https://blog.tianpei.cool',
+    thumbnail: 'blog.tianpei.cool.png',
+  },
+  {
+    title: "Nin3's Blog",
+    href: 'https://nin3-lee.netlify.app/',
+    thumbnail: 'nin3-lee.netlify.app.png',
+  },
+  {
+    title: '小羊molly',
+    href: 'https://mollypan.tech',
+    thumbnail: 'mollypan.tech.png',
+  },
+];
