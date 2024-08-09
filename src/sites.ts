e3489aa7b54d438af32543e73fd277d7f23b3a36
liruifengv <liruifeng1024@gmail.com>
2024-08-09 09:57:26 +0800

    fix format

--- src/sites.ts
+++ src/sites.ts
@@ -244,19 +244,19 @@ export const sites: Site[] = [
 		href: "https://qbb.sh",
 		thumbnail: "qbb.sh.png",
 	},
-	{ 
-		title: "RockyThink", 
-		href: "https://blog.rockythink.work", 
-		thumbnail: "blog.rockythink.work.png" 
+	{
+		title: "RockyThink",
+		href: "https://blog.rockythink.work",
+		thumbnail: "blog.rockythink.work.png",
 	},
-	{ 
-		title: "EthanLoo's", 
-		href: "https://blog.ethanloo.cn", 
-		thumbnail: "ethanloo.cn.png" 
+	{
+		title: "EthanLoo's",
+		href: "https://blog.ethanloo.cn",
+		thumbnail: "ethanloo.cn.png",
 	},
 	{
 		title: "Ani Offical website",
 		href: "https://myani.org",
-		thumbnail: "myani.org.png"
+		thumbnail: "myani.org.png",
 	},
 ];
