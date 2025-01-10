d74bef846e2d8fc3a5203658ff6afe78ba90981f
liruifengv <liruifeng1024@gmail.com>
2025-01-10 16:54:26 +0800

    重新部署

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,9 +4,13 @@ console.log("import.meta.env.SUPABASE_URL", import.meta.env.SUPABASE_URL);
 console.log("import.meta.env.SUPABASE_ANON_KEY", import.meta.env.SUPABASE_ANON_KEY);
 console.log("import.meta.env.SUPABASE_ROLE_KEY", import.meta.env.SUPABASE_ROLE_KEY);
 console.log("process.env.SUPABASE_ROLE_KEY", process.env.SUPABASE_ROLE_KEY);
+const SUPABASE_URL = process.env.SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY as string;
+const SUPABASE_ROLE_KEY = process.env.SUPABASE_ROLE_KEY as string;
+
 export const supabase = createClient(
-	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_ANON_KEY,
+	SUPABASE_URL,
+	SUPABASE_ANON_KEY,
 	{
 		auth: {
 			flowType: "pkce",
@@ -15,8 +19,8 @@ export const supabase = createClient(
 );
 
 export const supabaseAdmin = createClient(
-	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_ROLE_KEY,
+	SUPABASE_URL,
+	SUPABASE_ROLE_KEY,
 	{
 		auth: {
 			autoRefreshToken: false,
