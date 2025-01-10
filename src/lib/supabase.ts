f271c722d6253bab444973e0eab702a10f22443b
liruifengv <liruifeng1024@gmail.com>
2025-01-10 16:48:50 +0800

    重新部署

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 console.log("import.meta.env.SUPABASE_URL", import.meta.env.SUPABASE_URL);
 console.log("import.meta.env.SUPABASE_ANON_KEY", import.meta.env.SUPABASE_ANON_KEY);
 console.log("import.meta.env.SUPABASE_ROLE_KEY", import.meta.env.SUPABASE_ROLE_KEY);
+console.log("process.env.SUPABASE_ROLE_KEY", process.env.SUPABASE_ROLE_KEY);
 export const supabase = createClient(
 	import.meta.env.SUPABASE_URL,
 	import.meta.env.SUPABASE_ANON_KEY,
