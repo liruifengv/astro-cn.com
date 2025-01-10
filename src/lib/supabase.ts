5e9c9932155155b8caa09857fb508f4b80020632
liruifengv <liruifeng1024@gmail.com>
2025-01-10 15:44:11 +0800

    test: 打印调试 cloudflare 环境变量

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("import.meta.env.SUPABASE_URL", import.meta.env.SUPABASE_URL);
+console.log("import.meta.env.SUPABASE_ANON_KEY", import.meta.env.SUPABASE_ANON_KEY);
+console.log("import.meta.env.SUPABASE_ROLE_KEY", import.meta.env.SUPABASE_ROLE_KEY);
 export const supabase = createClient(
 	import.meta.env.SUPABASE_URL,
 	import.meta.env.SUPABASE_ANON_KEY,
