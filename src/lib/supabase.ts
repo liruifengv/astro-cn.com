9d6e64189b1d5f33aaea3b9e203ec39ea96b7c6e
liruifengv <liruifeng1024@gmail.com>
2025-01-10 17:33:51 +0800

    重新部署

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,21 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("import.meta.env.SUPABASE_URL", import.meta.env.SUPABASE_URL);
-console.log("import.meta.env.SUPABASE_ANON_KEY", import.meta.env.SUPABASE_ANON_KEY);
-console.log("process.env.SUPABASE_URL", process.env.SUPABASE_URL);
-console.log("process.env.SUPABASE_ANON_KEY", process.env.SUPABASE_ANON_KEY);
-
-
 import {
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_ANON_KEY,
+  SUPABASE_ROLE_KEY
  } from 'astro:env/server';
 
 console.log("SUPABASE_URL", SUPABASE_URL);
 console.log("SUPABASE_ANON_KEY", SUPABASE_ANON_KEY);
-
-//  const SUPABASE_URL = getSecret('SUPABASE_URL');
-//   const SUPABASE_ANON_KEY = getSecret('SUPABASE_ANON_KEY');
 
 export const supabase = createClient(
 	SUPABASE_URL,
@@ -27,13 +19,13 @@ export const supabase = createClient(
 	},
 );
 
-// export const supabaseAdmin = createClient(
-// 	import.meta.env.SUPABASE_URL,
-// 	import.meta.env.SUPABASE_ROLE_KEY,
-// 	{
-// 		auth: {
-// 			autoRefreshToken: false,
-// 			persistSession: false,
-// 		},
-// 	},
-// );
+export const supabaseAdmin = createClient(
+	SUPABASE_URL,
+	SUPABASE_ROLE_KEY,
+	{
+		auth: {
+			autoRefreshToken: false,
+			persistSession: false,
+		},
+	},
+);
