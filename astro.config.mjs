26d02664543ea7e26210771c9885359f994b4fd9
liruifengv <liruifeng1024@gmail.com>
2025-01-13 11:30:03 +0800

    删除无用项

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, envField, passthroughImageService } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 import icon from "astro-icon";
