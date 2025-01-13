7452c1a9ca5dacf99518450731ed80b38ab7f9e2
liruifengv <liruifeng1024@gmail.com>
2025-01-13 13:58:49 +0800

    chore: format

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js"
 
 import {
-	SUPABASE_URL,
 	SUPABASE_ANON_KEY,
 	SUPABASE_ROLE_KEY,
+	SUPABASE_URL,
 } from "astro:env/server"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
