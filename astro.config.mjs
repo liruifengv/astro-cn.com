927eb8e0c0cb42b1ea4873a8c3f5cb7859daf680
liruifengv <liruifeng1024@gmail.com>
2025-01-13 11:36:10 +0800

    fix deploy failed

--- astro.config.mjs
+++ astro.config.mjs
@@ -88,5 +88,14 @@ export default defineConfig({
         access: "secret",
       }),
     },
+  },
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   }
 });
