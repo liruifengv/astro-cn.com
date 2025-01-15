ce5c4b6e1e5bfe1413d592d7cd6582b5422b2b49
liruifengv <liruifeng1024@gmail.com>
2025-01-15 10:43:44 +0800

    fix: 修改中间件

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -7,37 +7,44 @@ const redirectRoutes = ["/signin", "/register"]
 export default defineMiddleware(
 	async ({ locals, url, redirect, rewrite, callAction }, next) => {
 		const pathname = url.pathname.replace(/\/$/, "") || "/"
-		if (protectedRoutes.includes(pathname)) {
-			const { data: user, error: getUserError } = await callAction(
-				actions.auth.getUser,
-				{},
-			)
-			if (getUserError) {
+
+		const { data: user, error: getUserError } = await callAction(
+			actions.auth.getUser,
+			{},
+		)
+		if (getUserError) {
+			locals.user = null
+
+			if (getUserError.code === "FORBIDDEN") {
+				console.error(
+					`用户被封禁【${getUserError.code}】${getUserError.message}`,
+				)
+				return redirect("/banned")
+			}
+			if (protectedRoutes.includes(pathname)) {
 				console.error(
 					`获取用户信息失败 【${getUserError.code}】${getUserError.message}`,
 				)
-				locals.user = null
-				if (getUserError.code === "FORBIDDEN") {
-					return redirect("/banned")
-				}
-
 				if (getUserError.code === "UNAUTHORIZED") {
 					return redirect("/signin")
 				}
-
 				return rewrite("/500")
 			}
+			return next()
+		}
 
-			locals.user = user
+		locals.user = user
 
-			if (!user.is_admin) {
-				return redirect("/")
-			}
+		if (!user.is_admin && protectedRoutes.includes(pathname)) {
+			return redirect("/")
 		}
 
 		if (redirectRoutes.includes(pathname)) {
 			console.log("redirectRoutes")
 			console.log("locals.user", locals.user)
+			if (locals.user) {
+				return redirect("/")
+			}
 		}
 
 		return next()
