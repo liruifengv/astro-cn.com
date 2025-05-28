366f4205a31ef478a36e7822dc02bae72761d792
liruifengv <liruifeng1024@gmail.com>
2025-05-28 17:08:13 +0800

    fix: fix format

--- src/sites.ts
+++ src/sites.ts
@@ -276,7 +276,8 @@ export const sites: Site[] = [
 		title: "Neko0xff Tech Blog",
 		href: "https://neko-0xff-blog.deno.dev",
 		thumbnail: "neko-0xff-blog.deno.dev.png",
-	},	{
+	},
+	{
 		title: "皮普的数字花园",
 		href: "https://pipuwong.com",
 		thumbnail: "pipuwong.com.png",
